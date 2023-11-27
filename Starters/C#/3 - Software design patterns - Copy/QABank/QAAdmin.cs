e50c15645adb8340e3a27f1e82f10ae22f63269b
AndySmithQA <andy.smith@qa.com>
2023-11-27 08:50:07 +0000

    first commit

---
+++ Starters/C#/3 - Software design patterns - Copy/QABank/QAAdmin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class QAAdmin  // CUT
+{
+    public bool enrolStudent(int id, IQADatabase db)
+    {
+        Student stu = db.getStudentByID(id);
+
+        // code to enrol studemt which we want to test
+
+        return true;
+    }
+}
---
+++ Starters/C#/3 - Software design patterns - Copy/QABankTests/QAStubDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class QAStubDatabase : IQADatabase
+{
+    List<Student> students = new List<Student>()
+        {
+            new Student(1,"London"), new Student(2,"Edinburgh"),
+            new Student(3,"London"), new Student(4,"Leeds"),
+            new Student(11,"Birmingham"), new Student(12,"Bristol"),
+            new Student(3,"Bristol"), new Student(4,"Coventry")
+        };
+
+    public Student getStudentByID(int id)
+    {
+        return students.Single(stu => stu.ID == id);
+    }
+
+    public List<Student> getStudentsByCity(string city)
+    {
+        return students.Where(stu => stu.City == city).ToList();
+    }
+}
