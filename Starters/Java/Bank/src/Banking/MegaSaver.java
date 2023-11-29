26fe378cdbd15ead8a7566dcdca2e0aba63bca84
AndySmithQA <andy.smith@qa.com>
2023-11-29 15:11:42 +0000

    Code Smells

--- Starters/Java/Bank/src/Banking/MegaSaver.java
+++
@@ -1,8 +0,0 @@
-package Banking;
-
-public class MegaSaver {
-    public static void main(String[] args) {
-        Savings cust2 = new Savings("Bob", 88);
-        System.out.println(cust2.getAccnum());
-    }
-}
